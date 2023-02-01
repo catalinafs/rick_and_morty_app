6c87b83cd799019c12b45408f053d29614b5d0b9
s3b4sbul4 <s3b4sbul4@gmmail.com>
2023-02-01 14:29:44 -0500

    jesuscommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Layout from "./components/Layout";
 
 function App() {
+  const [state, setState]= useState([])
+    console.log({state})
+  const loginUser = true
+  const url = 'https://rickandmortyapi.com/api/character'
+  useEffect(() => {
+    fetch(url).then(answer => answer.json()).then(answerData => {
+      setState(answerData.results)
+    })
+  }, [loginUser])
+  
   return (
     <div className='App'>
-      <h1>Rick and Morty App</h1>
-      <Layout/>
+      
+      <Layout>
+        
+
+        {/* <Header loginUser={loginUser} /> */}
+      
+        {/* <Footer /> */}
+        
+      </Layout>
     </div>
   );
 }
