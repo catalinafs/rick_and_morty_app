7e9983945cd7ed945b849774cf4c160b2ecab094
catalina <catalinaforerosuarez@gmail.com>
2023-01-31 20:29:05 -0500

    added paragraph

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 function App() {
   return (
     <div className="App">
-      <h1>Rick and Morty App</h1>  
-      <h1>Rick and Morty App</h1>  
-      <h1>Rick and Morty App</h1>  
-      <h1>Rick and Morty App</h1>  
-      <h1>Rick and Morty App</h1>  
-      <h1>Rick and Morty App</h1>  
+      <h1>Rick and Morty App</h1>
+      <p>
+        loredjkshaJFLKSGkuifgsedakfjygsvaekufyaesgkufeuaskfgaaaaaaaaaaaaaaaaeuskuaeykuasgfkueygfkeygfaskhfdeuwiyreiuafyr4woictyalkjfñls,mlñkfddskñlfmsdlkfjdsiñuotrebt6562gh2g1h8g6hg3h12fgh54fg65hf3gh2fg1hf85ghj+6fgj50ufmy2ifuy82im87fi1mt7d81im,9815e687mylokñmumiraeot8muaybertnilueyrtlbiue
+      </p>
     </div>
   );
 }
