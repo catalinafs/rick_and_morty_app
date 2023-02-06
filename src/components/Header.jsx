b1ad06a01c3b3ac774c81c3c51224b8c847bbdc6
jufraas <jufracamachoposso@gmail.com>
2023-02-06 18:44:10 -0500

    commit jufraas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Header = ({ children }) => {
     return (<>
     <div className="header">
+       {children}
         <input className="inputName" placeholder="search"></input>
     </div>
     </>);
