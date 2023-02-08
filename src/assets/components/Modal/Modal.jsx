6f2cdf4321ac010f25ad7d71bd25155856864290
catalina <catalinaforerosuarez@gmail.com>
2023-02-07 19:31:40 -0500

    modal code

--- src/assets/components/Modal/Modal.jsx
+++ src/assets/components/Modal/Modal.jsx
@@ -6,18 +6,19 @@ const Modal = () => {
 
     return (
         <>
-          <button onClick={() => setIsOpen(!isOpen)}>Open</button>
+          <button onClick={() => setIsOpen(!isOpen)}>Coders</button>
           {isOpen && (
-            <div className="Modal">
-              <div className="ModalContent">
-                <div className="ModalHeader">
-                  <h2>Programadores</h2>
-                </div>
-                <div className="ModalBody">
-                  Jufra,
-                  Sebastian,
-                  Catalina
-                  <button onClick={() => setIsOpen(!isOpen)}>Close</button>
+            <div className="MainContainerModal">
+
+              <div className="ContainerModal">
+                <button onClick={() => setIsOpen(!isOpen)}>X</button>
+
+                <h2>Programadores</h2>
+
+                <div className="Modal">
+                  <h2>Juan Francisco</h2>
+                  <h2>Sebastian Bula</h2>
+                  <h2>Catalina Forero</h2>
                 </div>
               </div>
             </div>
